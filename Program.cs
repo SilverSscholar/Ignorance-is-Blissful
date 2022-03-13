399ba6ef220512ceb898e9151f4057e8864763f0
Steve Jones <jones.wiredbraincoffee@gmail.com>
2022-03-13 13:11:59 -0400

    IntroScene finished

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ignorance_is_Blissful
+{
+    class Program
+    {
+        //ask for players name
+        //allow players name to be recalled through text
+        //introduce player to the storyline
+        //introduce a combat class
+        //introduce a player class
+        //call dialogue from a .txt file 
+
+        static void Main(string[] args)
+        {
+            IntroScreen();
+            DialogueOne();
+        }
+        //starting scene
+        static public void IntroScreen()
+        {
+            Console.WriteLine("Ignorance is Blissful");
+            Console.WriteLine("A C# program written by: Trinity Prichard");
+            Console.WriteLine("Through-out the length of this application please use the 'Enter' key to proceed.");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        static public void DialogueOne()
+        {
+
+        }
+
+    }
+}
